f7fb29db4831f01e2f8f7a84f02c40691a5b4a99
samhuang <imidya@gmail.com>
2015-01-13 14:15:51 +0800

    finish feature test

--- features/calculator.feature
+++ features/calculator.feature
@@ -1,18 +1,31 @@
 Feature: Calculator Functions
-	Test basic function of web calculator.
+	In order to complete ALSD this semester
+    As a student
+    I want to create a simple calculator to do some simple calculation for the final project.
 
-	Scenario: Test basic plus formula
-	Given A web calculator
-	When enter 1 + 2
-	Then get the result is 3
+	Scenario: Add function
+    	Given A web calculator
+    	When enter 1 + 2
+    	Then get the result is 3
 
-    Scenario: Test basic plus formula and some questions
-    Given A web calculator again
-    When enter <augend> + <addend>
-        | augend | addend |
-        |  8     |  4     |
-        |  5     |  9     |
+    Scenario: Times funciton
+        Given A web calculator
+        When enter 5 x 2
+        Then get the result is 10
 
-    Then get the sum is <sum>
-        | sum |
-        |  14 |
+    Scenario: Times funciton
+        Given A web calculator
+        When enter 5 + 4 x 3 - 9 / 3
+        Then get the result is 14
+
+    # Scenario: Test basic plus formula and some questions
+    # Given A web calculator again
+    # When enter <augend> + <addend>
+    #     | augend | addend |
+    #     |  8     |  4     |
+    #     |  5     |  9     |
+
+    # Then get the sum is <sum>
+    #     | sum |
+    #     |  14 |
+
--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -26,35 +26,68 @@ Then(/^get the result is (\d+)$/) do |arg1|
     browser.close
 end
 
-
-
-Given(/^A web calculator again$/) do
-    browser = Watir::Browser.new    
-    browser.goto 'http://127.0.0.1:5000'
-    browser.send_keys :f11
+When(/^enter (\d+) x (\d+)$/) do |arg1, arg2|
+    browser.div(:id, 'btn-' + arg1).click
+    sleep(1)
+    browser.div(:id, 'btn-times').click
+    sleep(1)
+    browser.div(:id, 'btn-' + arg2).click
+    sleep(1)
+    browser.div(:id, 'btn-equal').click
+    sleep(1)
 end
 
-When(/^enter <augend> \+ <addend>$/) do |table|
-    table.hashes.each do |record|
-        record[:augend].split('') do |char|
-            browser.div(:id, 'btn-' + char).click
-            sleep(1)
-        end
+When(/^enter (\d+) \+ (\d+) x (\d+) \- (\d+) \/ (\d+)$/) do |arg1, arg2, arg3, arg4, arg5|
+    browser.div(:id, 'btn-' + arg1).click
+    sleep(1)
+    browser.div(:id, 'btn-add').click
+    sleep(1)
+    browser.div(:id, 'btn-' + arg2).click
+    sleep(1)
+    browser.div(:id, 'btn-times').click
+    sleep(1)
+    browser.div(:id, 'btn-' + arg3).click
+    sleep(1)
+    browser.div(:id, 'btn-minus').click
+    sleep(1)
+    browser.div(:id, 'btn-' + arg4).click
+    sleep(1)
+    browser.div(:id, 'btn-divided').click
+    sleep(1)
+    browser.div(:id, 'btn-' + arg5).click
+    sleep(1)
+    browser.div(:id, 'btn-equal').click
+    sleep(1)
+end
+
+
+# Given(/^A web calculator again$/) do
+#     browser = Watir::Browser.new    
+#     browser.goto 'http://127.0.0.1:5000'
+#     browser.send_keys :f11
+# end
+
+# When(/^enter <augend> \+ <addend>$/) do |table|
+#     table.hashes.each do |record|
+#         record[:augend].split('') do |char|
+#             browser.div(:id, 'btn-' + char).click
+#             sleep(1)
+#         end
         
-        browser.div(:id, 'btn-add').click
-        sleep(1)
+#         browser.div(:id, 'btn-add').click
+#         sleep(1)
 
-        record[:addend].split('') do |char|
-            browser.div(:id, 'btn-' + char).click
-            sleep(1)
-        end
+#         record[:addend].split('') do |char|
+#             browser.div(:id, 'btn-' + char).click
+#             sleep(1)
+#         end
 
-        browser.div(:id, 'btn-equal').click
-        sleep(1)
-    end
-end
+#         browser.div(:id, 'btn-equal').click
+#         sleep(1)
+#     end
+# end
 
-Then(/^get the sum is <sum>$/) do |table|
-    expect(browser.span(:id => 'result-int').text).to eq table.hashes()[0][:sum]
-    browser.close
-end
+# Then(/^get the sum is <sum>$/) do |table|
+#     expect(browser.span(:id => 'result-int').text).to eq table.hashes()[0][:sum]
+#     browser.close
+# end
